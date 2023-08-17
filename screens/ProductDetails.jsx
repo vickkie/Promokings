70605a930b25c6ad2dfc354893ac8b12ac042844
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-08-17 03:06:29 +0100

    update

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,14 +1,28 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import './ProductDetails.style';
-import styles from './ProductDetails.style';
+import { View, Text } from "react-native";
+import React from "react";
+import "./ProductDetails.style";
+import styles from "./ProductDetails.style";
+import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity, Image } from "react-native";
+import { COLORS } from "../constants";
 
-const ProductDetails = () => {
+const ProductDetails = ({navigation}) => {
   return (
     <View style={styles.container}>
-      <Text>ProductDetails</Text>
+      <View style={styles.upperRow}>
+        <TouchableOpacity onPress={() => navigation.goBack() }>
+          <Ionicons size={30} name="chevron-back-circle" />
+        </TouchableOpacity>
+
+        <TouchableOpacity onPress={() => {} }>
+          <Ionicons size={30} name="heart" color={COLORS.primary} />
+        </TouchableOpacity>
+      </View>
+
+      <Image style={styles.image}
+       source={{uri: "https://media.istockphoto.com/id/1373329869/photo/modern-living-room-interior-3d-render.webp?b=1&s=170667a&w=0&k=20&c=npJvK3ITd99aF715CH3dzceECPdpzNhlnhBjv_xzL0Q="}} />
     </View>
-  )
-}
+  );
+};
 
 export default ProductDetails;
--- screens/ProductDetails.style.js
+++ screens/ProductDetails.style.js
@@ -4,6 +4,22 @@ import { SIZES, COLORS } from "../constants";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+    },
+
+    upperRow: {
+        marginHorizontal: 20,
+        flexDirection: "row",
+        justifyContent: 'space-between',
+        alignItems: "center",
+        position: "absolute",
+        top: SIZES.xxLarge,
+        width: SIZES.width -44,
+        zIndex: 999
+    },
+
+    image: {
+        aspectRatio: 1,
+        resizeMode: "cover"
     }
 });
 
