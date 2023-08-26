c903aa5e3ab40d26fff19c6a454f0a78e070b758
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-08-26 01:48:59 +0100

    update

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -1,26 +1,36 @@
-import { View, Text } from "react-native"
-import React from "react"
-import "./ProductDetails.style"
-import styles from "./ProductDetails.style"
+import { View, Text } from "react-native";
+import React from "react";
+import "./ProductDetails.style";
+import styles from "./ProductDetails.style";
 import { Ionicons } from "@expo/vector-icons";
-import { TouchableOpacity, Image } from "react-native"
-import { COLORS } from "../constants"
+import { TouchableOpacity, Image } from "react-native";
+import { COLORS } from "../constants";
 
-const ProductDetails = ({navigation}) => {
+const ProductDetails = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <View style={styles.upperRow}>
-        <TouchableOpacity onPress={() => navigation.goBack() }>
-          <Ionicons size={30} name='chevron-back-circle' />
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <Ionicons size={30} name="chevron-back-circle" />
         </TouchableOpacity>
 
-        <TouchableOpacity onPress={() => {} }>
+        <TouchableOpacity onPress={() => {}}>
           <Ionicons size={30} name="heart" color={COLORS.primary} />
         </TouchableOpacity>
       </View>
 
-      <Image style={styles.image}
-       source={{uri: "https://media.istockphoto.com/id/1373329869/photo/modern-living-room-interior-3d-render.webp?b=1&s=170667a&w=0&k=20&c=npJvK3ITd99aF715CH3dzceECPdpzNhlnhBjv_xzL0Q="}} />
+      <Image
+        style={styles.image}
+        source={{
+          uri: "https://media.istockphoto.com/id/1373329869/photo/modern-living-room-interior-3d-render.webp?b=1&s=170667a&w=0&k=20&c=npJvK3ITd99aF715CH3dzceECPdpzNhlnhBjv_xzL0Q=",
+        }}
+      />
+
+      <View style={styles.details}>
+        <View style={styles.titleRow}>
+          <Text>Title Row</Text>
+        </View>
+      </View>
     </View>
   );
 };
--- screens/ProductDetails.style.js
+++ screens/ProductDetails.style.js
@@ -4,6 +4,7 @@ import { SIZES, COLORS } from "../constants";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
+        backgroundColor: COLORS.lightWhite
     },
 
     upperRow: {
@@ -13,13 +14,31 @@ const styles = StyleSheet.create({
         alignItems: "center",
         position: "absolute",
         top: SIZES.xxLarge,
-        width: SIZES.width -45,
+        width: SIZES.width - 45,
         zIndex: 999
     },
 
     image: {
         aspectRatio: 1,
         resizeMode: "cover"
+    },
+
+    details: {
+        marginTop: -SIZES.large,
+        backgroundColor: COLORS.lightWhite,
+        width: SIZES.width,
+        borderTopLeftRadius: SIZES.medium,
+        borderTopRightRadius: SIZES.medium,
+    },
+
+    titleRow: {
+        marginHorizontal: 20,
+        paddingBottom: SIZES.small,
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: SIZES.width - 44,
+        top: 20
     }
 });
 
