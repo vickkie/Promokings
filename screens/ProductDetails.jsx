a177aafdab6c18dd33e1c24a25ddff5f6e4262cc
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-09-06 15:30:10 +0100

    update

--- screens/ProductDetails.jsx
+++ screens/ProductDetails.jsx
@@ -74,9 +74,7 @@ const ProductDetails = ({ navigation }) => {
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
             remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            with the release of Letraset sheets containing Lorem Ipsum passages
           </Text>
         </View>
 
