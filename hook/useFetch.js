466197f4bee6f5104f8e7ed5c40efba20cf541d2
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-09-22 17:44:54 +0100

    update

--- hook/useFetch.js
+++ hook/useFetch.js
@@ -1,12 +1,41 @@
 import { View, Text } from 'react-native';
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
 
 const useFetch = () => {
-  return (
-    <View>
-      <Text>useFetch</Text>
-    </View>
-  )
+
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(false);
+
+    const fetchData = async () => {
+        setIsLoading(true);
+
+        try {
+            const response = await axios.get('http://localhost:3000/api/products/');
+            setData(response.data);
+            setIsLoading(false);
+
+        } catch (error) {
+            setError(error)
+        } finally {
+            setIsLoading(false);
+        }
+
+    }
+
+    useEffect(() => {
+        fetchData();
+    });
+
+    const refetch = () => {
+        setIsLoading(true);
+        fetchData();
+    }
+
+
+    return { data, isLoading, error, refetch };
 }
 
-export default useFetch
+export default useFetch;
