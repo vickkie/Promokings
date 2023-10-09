cb4ff7ac3b8a3969df8e2bd1041c8b5516215b39
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-10-09 01:30:39 +0100

    section 2

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { SafeAreaView } from 'react-native';
+import { SafeAreaView } from  "react-native-safe-area-context";
 
 const Profile = () => {
   return (
