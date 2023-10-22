042c896ec1c9cff555cff7745bc3b8fe3b82eb47
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-10-22 02:11:21 +0100

    update

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -1,13 +1,35 @@
-import { View, Text } from 'react-native';
-import React from 'react';
-import { SafeAreaView } from  "react-native-safe-area-context";
-
+import { View, Text, Image } from "react-native";
+import { useState, useEffect } from "react";
+import styles from "./Profile.style";
+import { StatusBar } from "expo-status-bar";
+import { COLORS } from "../constants";
 const Profile = () => {
+
+  const [userData, setUserData] = useState(null);
+  const [userLogin, setUserLogin] = useState(false);
+
   return (
-    <SafeAreaView>
-      <Text>Profile</Text>
-    </SafeAreaView>
-  )
-}
+    <View style={styles.container}>
+      <View style={styles.container}>
+        <StatusBar backgroundColor="transparent" />
+        <View style={{ width: "100%" }}>
+          <Image
+            source={require("../assets/images/space.jpg")}
+            style={styles.cover}
+          />
+        </View>
+
+        <View style={styles.profileContainer}>
+          <Image
+            source={require("../assets/images/profile.jpeg")}
+            style={styles.profile}
+          />
+
+          <Text style={styles.name}>Maaz junior</Text>
+        </View>
+      </View>
+    </View>
+  );
+};
 
 export default Profile;
--- screens/Profile.style.js
+++ screens/Profile.style.js
@@ -4,6 +4,38 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 
+    container: {
+        backgroundColor: COLORS.lightWhite,
+        flex: 1,
+    },
+
+    cover: {
+        height: 290,
+        width: "100%",
+        resizeMode: "cover"
+    },
+
+    profileContainer: {
+        flex: 1,
+        alignItems: 'center'
+    },
+
+    profile: {
+        height: 155,
+        width: 155,
+        borderRadius: 999,
+        borderColor: COLORS.primary,
+        borderWidth: 2,
+        resizeMode: "cover",
+        marginTop: -90
+    },
+
+    name: {
+        fontFamily: "bold",
+        color: COLORS.primary,
+        marginVertical: 6
+    }
+
 });
 
 export default styles;
