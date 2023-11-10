e5bab7ec43d1be3926086c8d27db8c42ba81865c
goalmaaz@gmail.com <goalmaaz@gmail.com>
2023-11-10 04:15:17 +0100

    update

---
+++ screens/Login.style.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+import {COLORS, SIZES} from '../constants';
+
+const styles = StyleSheet.create({
+
+});
+
+export default styles;
--- screens/LoginPage.jsx
+++ screens/LoginPage.jsx
@@ -1,5 +1,6 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView, TouchableOpacity } from 'react-native'
+import React from 'react';
+
 
 const LoginPage = () => {
   return (
