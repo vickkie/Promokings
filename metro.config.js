601981773cf590d2701a0d9ddaa823c104e18835
vickkie <vickkietrake@gmail.com>
2024-07-24 13:26:08 +0100

    not building

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,17 @@
-const { getDefaultConfig } = require("metro-config");
+// Import the default Metro config from Expo
+const { getDefaultConfig } = require("@expo/metro-config");
 
-module.exports = (async () => {
-  const {
-    resolver: { sourceExts, assetExts },
-  } = await getDefaultConfig();
-  return {
-    transformer: {
-      babelTransformerPath: require.resolve("react-native-svg-transformer"),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== "svg"),
-      sourceExts: [...sourceExts, "svg"],
-    },
-  };
-})();
+// Get the default Metro config
+const defaultConfig = getDefaultConfig(process.cwd());
+
+// Modify the transformer to include react-native-svg-transformer
+defaultConfig.transformer = {
+  ...defaultConfig.transformer,
+  babelTransformerPath: require.resolve("react-native-svg-transformer"),
+};
+
+// Adjust the resolver to handle SVG files correctly
+defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg");
+defaultConfig.resolver.sourceExts.push("svg");
+
+module.exports = defaultConfig;
