9edce655014f801976b62f9e8ea878d250e7383e
vickkie <vickkietrake@gmail.com>
2024-07-30 12:03:27 +0100

    step2

--- screens/Checkout.jsx
+++ screens/Checkout.jsx
@@ -1,22 +1,57 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { View, Text, Button, TextInput, StyleSheet, TouchableOpacity, ScrollView, Image } from "react-native";
 import { useRoute, useNavigation } from "@react-navigation/native";
 import Icon from "../constants/icons";
 import { SIZES, COLORS } from "../constants";
 import { useCart } from "../contexts/CartContext";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { AuthContext } from "../components/auth/AuthContext";
+import * as Yup from "yup";
 
 const Checkout = () => {
   const route = useRoute();
   const navigation = useNavigation();
   const { estimatedAmount, products, totals, additionalFees } = route.params;
+  const { userData, userLogin } = useContext(AuthContext);
+
+  useEffect(() => {
+    if (!userLogin) {
+      setUserId(1);
+      navigation.navigate("Login");
+      return;
+    } else if (userData && userData._id) {
+      setUserId(userData._id);
+    }
+  }, [userLogin, userData]);
 
   const [step, setStep] = useState(1);
   const [address, setAddress] = useState("");
   const [phoneNumber, setPhoneNumber] = useState("");
+  const [city, setCity] = useState(userData ? userData.location : "");
+  const [email, setEmail] = useState(userData ? userData.email : "");
   const { handleItemCountChange } = useCart();
+  const [userId, setUserId] = useState(null);
+  const [phoneError, setPhoneError] = useState("");
+
+  const checkoutSchema = Yup.object().shape({
+    phoneNumber: Yup.string()
+      .required("Phone number is required")
+      .matches(/^[0-9]+$/, "Phone number must contain only digits"),
+  });
+
+  useEffect(() => {
+    if (phoneNumber) {
+      checkoutSchema
+        .validate({ phoneNumber })
+        .then(() => setPhoneError(""))
+        .catch((err) => setPhoneError(err.errors[0]));
+    }
+  }, [phoneNumber]);
 
   const handleNext = () => {
-    if (step < 3) {
+    if (step === 2 && phoneError) {
+      setStep(step);
+    } else {
       setStep(step + 1);
     }
   };
@@ -31,121 +66,155 @@ const Checkout = () => {
     console.log("Payment processed");
   };
 
+  const navigateWhere = (locate) => {
+    if (step > 1) {
+      handlePrevious();
+    } else {
+      navigation.goBack();
+    }
+  };
+
   return (
-    <View style={styles.container}>
-      <View style={styles.wrapper}>
-        <View style={styles.upperRow}>
-          <View style={styles.upperButtons}>
-            <TouchableOpacity onPress={() => navigation.goBack()} style={[styles.backBtn, styles.buttonWrap]}>
-              <Icon name="backbutton" size={26} />
-            </TouchableOpacity>
-            <Text style={styles.topheading}>Checkout</Text>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.container}>
+        <View style={styles.wrapper}>
+          <View style={styles.upperRow}>
+            <View style={styles.upperButtons}>
+              <TouchableOpacity onPress={navigateWhere} style={[styles.backBtn, styles.buttonWrap]}>
+                <Icon name="backbutton" size={26} />
+              </TouchableOpacity>
+              <Text style={styles.topheading}>Checkout</Text>
+            </View>
+            <View style={styles.paginationContainer}>
+              <View style={styles.dot(step === 1 ? COLORS.themey : COLORS.themeg)} />
+              <View style={styles.dot(step === 2 ? COLORS.themey : COLORS.themeg)} />
+              <View style={styles.dot(step === 3 ? COLORS.themey : COLORS.themeg)} />
+            </View>
+            <View style={styles.stepsheader}>
+              <Text style={styles.stepstext}>Just a couple of steps and you good to go</Text>
+            </View>
           </View>
-          <View style={styles.paginationContainer}>
-            <View style={styles.dot(step === 1 ? COLORS.themey : COLORS.themeg)} />
-            <View style={styles.dot(step === 2 ? COLORS.themey : COLORS.themeg)} />
-            <View style={styles.dot(step === 3 ? COLORS.themey : COLORS.themeg)} />
-          </View>
-          <View style={styles.stepsheader}>
-            <Text style={styles.stepstext}>Just a couple of steps and you good to go</Text>
-          </View>
-        </View>
 
-        <View style={styles.lowerRow}>
-          <ScrollView>
-            {step === 1 && (
-              <View style={styles.stepContainer}>
-                <View style={{ justifyContent: "center", alignItems: "center", padding: 20 }}>
-                  <Icon name="checkout" size={26} />
-                </View>
+          <View style={styles.lowerRow}>
+            <ScrollView>
+              {step === 1 && (
+                <View style={styles.stepContainer}>
+                  <View style={{ justifyContent: "center", alignItems: "center", padding: 20 }}>
+                    <Icon name="checkout" size={26} />
+                  </View>
 
-                <View style={{ width: SIZES.width - 27 }}>
-                  {products.map((item) => (
-                    <View style={styles.containerx} key={item._id}>
-                      <TouchableOpacity
-                        style={styles.imageContainer}
-                        onPress={() => navigation.navigate("ProductDetails", { item, itemid: item._id })}
-                      >
-                        <Image source={{ uri: item.cartItem.imageUrl }} style={styles.image} />
-                      </TouchableOpacity>
-                      <View style={{ gap: 12 }}>
-                        <View style={styles.details}>
-                          <Text style={styles.title} numberOfLines={1}>
-                            {item.cartItem.title}
-                          </Text>
-                        </View>
-                        <View style={styles.rowitem}>
-                          <View style={styles.xp}>
-                            <Text style={styles.semititle} numberOfLines={1}>
-                              SIZE-{item.size}
+                  <View style={{ width: SIZES.width - 27 }}>
+                    {products.map((item) => (
+                      <View style={styles.containerx} key={item._id}>
+                        <TouchableOpacity
+                          style={styles.imageContainer}
+                          onPress={() => navigation.navigate("ProductDetails", { item, itemid: item._id })}
+                        >
+                          <Image source={{ uri: item.cartItem.imageUrl }} style={styles.image} />
+                        </TouchableOpacity>
+                        <View style={{ gap: 12 }}>
+                          <View style={styles.details}>
+                            <Text style={styles.title} numberOfLines={1}>
+                              {item.cartItem.title}
                             </Text>
                           </View>
-                        </View>
-                        <View style={styles.rowitem}>
-                          <View>
-                            <Text style={styles.semititle}>Quantity : {item.quantity}</Text>
+                          <View style={styles.rowitem}>
+                            <View style={styles.xp}>
+                              <Text style={styles.semititle} numberOfLines={1}>
+                                SIZE-{item.size}
+                              </Text>
+                            </View>
                           </View>
+                          <View style={styles.rowitem}>
+                            <View>
+                              <Text style={styles.semititle}>Quantity : {item.quantity}</Text>
+                            </View>
 
-                          <View style={styles.priceadd}>
-                            <Text style={styles.semititle}>
-                              Totals :
-                              {`KES ${new Intl.NumberFormat("en-US", { style: "currency", currency: "KES" })
-                                .format(totals[item._id])
-                                .replace("KES", "")
-                                .trim()}`}
-                            </Text>
+                            <View style={styles.priceadd}>
+                              <Text style={styles.semititle}>
+                                Totals :
+                                {`KES ${new Intl.NumberFormat("en-US", { style: "currency", currency: "KES" })
+                                  .format(totals[item._id])
+                                  .replace("KES", "")
+                                  .trim()}`}
+                              </Text>
+                            </View>
                           </View>
                         </View>
                       </View>
+                    ))}
+                  </View>
+
+                  <View>
+                    <Text style={styles.amount}>Estimated Amount: {estimatedAmount}</Text>
+                    <TouchableOpacity style={styles.button1} onPress={handleNext}>
+                      <Text style={styles.nextText}>Next</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+              {step === 2 && (
+                <View style={styles.stepContainer}>
+                  <View style={styles.shippingContainer}>
+                    <Text style={styles.label}>Shipping City Name</Text>
+                    <TextInput
+                      style={[styles.input]}
+                      placeholder={userData.location}
+                      value={userData.location}
+                      onChangeText={setCity}
+                    />
+                    <Text style={styles.label}>Shipping Address</Text>
+                    <TextInput
+                      style={styles.input}
+                      placeholder="Enter address"
+                      value={address}
+                      onChangeText={setAddress}
+                    />
+                    <Text style={styles.label}>Shipping email</Text>
+                    <TextInput
+                      style={styles.input}
+                      placeholder={userData.email}
+                      value={email}
+                      onChangeText={setEmail}
+                    />
+
+                    {phoneError ? <Text style={styles.error}>{phoneError}</Text> : null}
+
+                    <Text style={styles.label}>Phone Number</Text>
+                    <TextInput
+                      style={styles.input}
+                      placeholder="Enter phone number"
+                      value={phoneNumber}
+                      onChangeText={(text) => setPhoneNumber(text)}
+                    />
+
+                    <View style={styles.next2wrapper}>
+                      <TouchableOpacity onPress={handlePrevious} style={styles.previous}>
+                        <Text>
+                          <Icon name="backbutton" size={36} />
+                        </Text>
+                      </TouchableOpacity>
+                      <TouchableOpacity onPress={handleNext} style={styles.next2}>
+                        <Text style={styles.buttonText}>Next step</Text>
+                      </TouchableOpacity>
                     </View>
-                  ))}
+                  </View>
                 </View>
+              )}
 
-                <View>
-                  <Text style={styles.amount}>Estimated Amount: {estimatedAmount}</Text>
-                  <TouchableOpacity style={styles.button1} onPress={handleNext}>
-                    <Text style={styles.nextText}>Next</Text>
-                  </TouchableOpacity>
+              {step === 3 && (
+                <View style={styles.stepContainer}>
+                  <Text>Payment Information</Text>
+                  {/* Display payment options */}
+                  <Button title="Previous" onPress={handlePrevious} />
+                  <Button title="Pay" onPress={handlePayment} />
                 </View>
-              </View>
-            )}
-            {step === 2 && (
-              <View style={styles.stepContainer}>
-                <Text>Shipping Address</Text>
-                <TextInput style={styles.input} placeholder="Enter address" value={address} onChangeText={setAddress} />
-                <Text>Phone Number</Text>
-                <TextInput
-                  style={styles.input}
-                  placeholder="Enter phone number"
-                  value={phoneNumber}
-                  onChangeText={setPhoneNumber}
-                />
-
-                <View style={styles.next2wrapper}>
-                  <TouchableOpacity onPress={handlePrevious} style={styles.previous}>
-                    <Text>
-                      <Icon name="backbutton" size={36} />
-                    </Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={handleNext} style={styles.next2}>
-                    <Text>Next step</Text>
-                  </TouchableOpacity>
-                </View>
-              </View>
-            )}
-
-            {step === 3 && (
-              <View style={styles.stepContainer}>
-                <Text>Payment Information</Text>
-                {/* Display payment options */}
-                <Button title="Previous" onPress={handlePrevious} />
-                <Button title="Pay" onPress={handlePayment} />
-              </View>
-            )}
-          </ScrollView>
+              )}
+            </ScrollView>
+          </View>
         </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
@@ -330,6 +399,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
     flexDirection: "row",
     gap: 10,
+    position: "absolute",
+    bottom: 20,
   },
   next2: {
     justifyContent: "center",
@@ -339,4 +410,30 @@ const styles = StyleSheet.create({
     borderRadius: SIZES.medium,
     height: 60,
   },
+  input: {
+    flexDirection: "row",
+    alignItems: "center",
+    backgroundColor: COLORS.themeg,
+    padding: 10,
+    borderRadius: SIZES.medium,
+    marginBottom: 10,
+    width: SIZES.width - 40,
+    marginStart: 5,
+  },
+  label: {
+    fontSize: SIZES.small,
+    marginBottom: SIZES.xSmall,
+    color: COLORS.gray,
+    marginStart: SIZES.large,
+  },
+  shippingContainer: {
+    minHeight: SIZES.height - 220,
+    marginTop: SIZES.xxLarge,
+  },
+  buttonText: {
+    color: COLORS.lightWhite,
+    fontFamily: "semibold",
+    fontSize: SIZES.medium,
+  },
+  error: { color: "red", marginBottom: 10, marginStart: 10 },
 });
